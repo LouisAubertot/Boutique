b8337490993e36fcfc4153d98f0dcd2e5f2fb7da
Aubertot <louis.aubertot@gmail.com>
2018-01-09 16:42:09 +0100

    Push Articles Cliens Command Getter Setter Attributs

--- app/src/main/java/com/example/weezy/ecommerce/Commande.java
+++ app/src/main/java/com/example/weezy/ecommerce/Commande.java
@@ -1,8 +1,23 @@
 package com.example.weezy.ecommerce;
 
+import java.util.Date;
+
 /**
  * Created by Weezy on 09/01/2018.
  */
 
 public class Commande {
+
+    private Date dateCommand;
+
+    public Date getDateCommand() {
+        return dateCommand;
+    }
+
+    public void setDateCommand(Date dateCommand) {
+        this.dateCommand = dateCommand;
+    }
+
+
+
 }
