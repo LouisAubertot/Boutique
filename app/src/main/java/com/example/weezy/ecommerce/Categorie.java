980d3bc8f91cfd47472853d7e3165dc743fcf2d5
LETANG Alexandre <letang.alexandre88@gmail.com>
2018-01-09 17:00:22 +0100

    Merge branch 'commerce' of https://github.com/LouisAubertot/Boutique into commerce
    # Conflicts:
    #	app/src/main/java/com/example/weezy/ecommerce/Article.java

--- app/src/main/java/com/example/weezy/ecommerce/Categorie.java
+++ app/src/main/java/com/example/weezy/ecommerce/Categorie.java
@@ -6,16 +6,16 @@ package com.example.weezy.ecommerce;
 
 public class Categorie
 {
-    private String nom_categorie;
+    private String nomCategorie;
 
 
-    public String getNom_categorie() {
-        return nom_categorie;
+    public String getNomCategorie() {
+        return nomCategorie;
     }
 
-    public void setNom_categorie(String nom_categorie) {
-        this.nom_categorie = nom_categorie;
+    public void setNomCategorie(String nomCategorie) {
+        this.nomCategorie = nomCategorie;
     }
 
-
+d
 }
