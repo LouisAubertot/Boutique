27f0f6bb26b581aaf21ff9eef4230f050d81fc22
LETANG Alexandre <letang.alexandre88@gmail.com>
2018-01-09 17:14:19 +0100

    Merge branch 'commerce' of https://github.com/LouisAubertot/Boutique into commerce
    # Conflicts:
    #	app/src/main/java/com/example/weezy/ecommerce/Article.java

--- app/src/main/java/com/example/weezy/ecommerce/Categorie.java
+++ app/src/main/java/com/example/weezy/ecommerce/Categorie.java
@@ -1,21 +1,29 @@
 package com.example.weezy.ecommerce;
 
-/**
- * Created by Weezy on 09/01/2018.
- */
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class Categorie
-{
-    private String nomCategorie;
+public class Categorie extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categorie);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    public String getNomCategorie() {
-        return nomCategorie;
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
-
-    public void setNomCategorie(String nomCategorie) {
-        this.nomCategorie = nomCategorie;
-    }
-
 
 }
