9fcf27d1a0d7e621b3a0f85fd158729acc93da09
Aubertot <louis.aubertot@gmail.com>
2018-01-12 13:58:48 +0100

    Permsission internet

--- app/src/main/java/com/example/weezy/ecommerce/Promotion.java
+++ app/src/main/java/com/example/weezy/ecommerce/Promotion.java
@@ -1,8 +1,30 @@
 package com.example.weezy.ecommerce;
 
+import java.util.Date;
+
 /**
  * Created by Weezy on 09/01/2018.
  */
 
 public class Promotion {
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public Float getPourcent() {
+        return pourcent;
+    }
+
+    public void setPourcent(Float pourcent) {
+        this.pourcent = pourcent;
+    }
+
+    private Date dateFin;
+    private Float pourcent;
+
 }
