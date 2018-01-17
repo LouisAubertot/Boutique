7caf3eed5a6523e491c658eb47230336e7205cd0
Aubertot <louis.aubertot@gmail.com>
2018-01-17 16:44:17 +0100

    Vla les changes mon gars

---
+++ app/src/main/java/com/example/weezy/ecommerce/Method/Categorie.java
@@ -0,0 +1,29 @@
+package com.example.weezy.ecommerce.Method;
+
+/**
+ * Created by Weezy on 17/01/2018.
+ */
+
+public class Categorie {
+
+    private String nomCateg;
+    private String visuel;
+
+    public String getNomCateg() {
+        return nomCateg;
+    }
+
+    public void setNomCateg(String nomCateg) {
+        this.nomCateg = nomCateg;
+    }
+
+    public String getVisuel() {
+        return visuel;
+    }
+
+    public void setVisuel(String visuel) {
+        this.visuel = visuel;
+    }
+
+
+}
